7c3dfbc5d7cc75958ed133b78908692e16943567
xiaonansun <42507167+xiaonansun@users.noreply.github.com>
2023-07-10 19:52:49 -0400

    2023-07-10 upload

--- 2pAnalysisCode/behavior_getStimEvents.m
+++ 2pAnalysisCode/behavior_getStimEvents.m
@@ -1,7 +1,6 @@
 function E = behavior_getStimEvents(bhv)
 %% Input: bhv is the Bpod behavior session structure (e.g. SessionData)
 
-events = bhv.stimEvents;
 
 %%
 % Auditory event timing. Each cell is a vector of event timing relative to
@@ -9,6 +8,7 @@ events = bhv.stimEvents;
 % Column 1: left-sided event timing 
 % column 2: right-sided event timing
 % Each row is a single trials
+events = bhv.stimEvents;
 eventTiming = cellfun(@(x) x{:},[cellfun(@(x) x(1),events,'UniformOutput',false)' cellfun(@(x) x(2),events,'UniformOutput',false)'],'UniformOutput',false); 
 
 % The of count the number of auditory events emerging from the left or right speakers and report them in two columns (column 1 for left and 2 for right)
